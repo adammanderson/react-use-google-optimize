049a5bb5cd38a0d5823d51094a5c2ab645a5a4b5
Adam Anderson <adam.anderson@roh.org.uk>
2019-06-11 15:50:02 +0100

    Add use-google-optimize hook

---
+++ src/use-google-optimize.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+export default function useGoogleOptimize(experimentID) {
+  const [variant, setVariant] = useState(null);
+  const updateVariant = (value) => {
+    setVariant(value === undefined || value === null ? '0' : value);
+  }
+
+  useEffect(() => {
+    const { dataLayer, google_optimize } = window;
+    const delayedInitialization = () => {
+      const value = google_optimize && google_optimize.get(experimentID);
+      updateVariant(value);
+    }
+
+    if (dataLayer) {
+      const hideEnd = dataLayer && dataLayer.hide && dataLayer.hide.end;
+
+      if (hideEnd) {
+        dataLayer.hide.end = () => {
+          delayedInitialization();
+          hideEnd();
+        }
+      } else {
+        delayedInitialization();
+      }
+
+      dataLayer.push('event', 'optimize.callback', {
+        name: experimentID,
+        callback: updateVariant,
+      });
+    }
+
+    return () => {
+      dataLayer.push('event', 'optimize.callback', {
+        name: experimentID,
+        callback: updateVariant,
+        remove: true,
+      });
+    }
+  })
+
+  return variant;
+}
